50f7056152ca82f27d6a4bc18b083ec88bbb785b
Ashanta Palei <ashanta_palei@epam.com>
2023-06-10 14:07:24 +0530

    Python script for data types

---
+++ Valaxy_Tech/Data_Types_Variables/data_types.py
@@ -0,0 +1,47 @@
+#Every value in python has a data type
+#Since everything is an object in python programming, data types are actually classes and variables are intance (object) of these classes.
+#These are various data types in Python
+
+#Basic Data Types are:
+
+    #Numbers (int, float and complex)
+    #Strings
+    #Boolean
+'''
+x=3
+y=4.8
+z=3+4j
+
+print(x,type(x)) #singel line print
+print(y,type(y)) #singel line print
+print(z,type(z)) #singel line print
+
+lan_name="python scripting" #Strings should be in quotations
+print(lan_name)
+
+'''
+
+my_value=True   #True is pre-defined variable
+my_newfalue=False   #False is pre-defined variable
+
+print(my_value,type(my_value))
+print(my_newfalue,type(my_newfalue))
+
+#=====================================================================
+#=====================================================================
+
+#Typecasting or Type Conversation
+
+x=3
+
+xy= print(str(x))
+
+
+
+
+
+
+
+
+
+
---
+++ Valaxy_Tech/Data_Types_Variables/input_output.py
@@ -0,0 +1,10 @@
+#Simple arithmetic calculator
+
+a= int (input("enter value for a : "))
+b= int (input("enter value for b : "))
+
+result=a+b
+print(f"The type of {a} is : {type(a)}")
+print(f"The type of {b} is : {type(b)}")
+print(f'The addition of {a} & {b} is : {result}')
+
