b1e2f5ac3837bb4fec69110c8bb8ae3fa7bc2406
ashantap <ashanta.palei@gmail.com>
2023-06-19 21:11:42 +0530

    commit -m

---
+++ Shell_Script/FIlter_Commands/basic_usages_grep_command/grep.sh
@@ -0,0 +1,10 @@
+grep "line1" "filename"     #This is find the word "line1" inside the "filename"
+grep "bash" *   #This will try to find "bash" in current locations (no matter how many files are there)
+
+
+cat text.txt | grep "above" #This command will try to find "above" in text.txt file
+
+
+echo "==================I============================"
+grep -i "Above" test.txt    #This option(-i) will ignore the case sensivity
+
