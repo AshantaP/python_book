e8231503a50e42610f6b9801d702f390582ebf18
ashantap <ashanta.palei@gmail.com>
2023-06-28 23:00:49 +0530

    config changes

--- Shell_Script/Operations_On_Strings/operations_on_strings_on_realtime/ops_on_str.sh
+++ Shell_Script/Operations_On_Strings/operations_on_strings_on_realtime/ops_on_str.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+httpd_file_name="etc/httpd/conf/httpd.conf"
+
 #Question-1: Finding the string lenth.
 echo "==================Finding the string lenth==============="
 my_name="Shell script is good for automation"
@@ -30,5 +32,38 @@ echo "Your capital name is : ${my_upper_case_name}"
 echo ${my_lower_case_name}
 
 #Question:4 Usage of dirname and basename on strings/paths
+echo "===Usage of dirname and basename on strings/paths"
+
+dirname /etc/httpd/conf/httpd.conf  #This command will print the dirname name without "httpd.conf"
+basename etc/httpd/conf/httpd.conf  #This command will print the last file name "httpd.config"
+
+#dirname : strip/removes last component from file name
+#basename : strip/removes directory and suffix from filenames
+
+
+httpd_dirname_file=$(dirname $httpd_file_name)
+
+httpd_basename_file=$(basename $httpd_file_name)
+
+echo ${httpd_basename_file}
+echo ${httpd_dirname_file}
+
+#Question:5 Slicing
+echo " =================Slicing==========================="
+my_name="MY NAME IS ASHANTA KUMAR PALEI"
+
+echo ${my_name}
+echo ${my_name:0}
+echo ${my_name:1}
+
+
+
+
+
+
+
+
+
+
 
 
