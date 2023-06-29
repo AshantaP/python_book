56b3b049d9935c74d6577c129d63a70d48d94413
ashantap <ashanta.palei@gmail.com>
2023-06-29 21:05:17 +0530

    config changes

---
+++ Python_Scripts/Working_with_Python_Modules/platform_Module/use_of_platform_module.py
@@ -0,0 +1,15 @@
+import platform
+print(f" The OS name is {platform.system()}")
+print(f"Python version is {platform.python_version()}")
+print(f"Python version is tupel is {platform.python_version_tuple()}")
+print(platform.machine())
+print(platform.platform())
+
+
+#print(dir(platform))
+#To get more insights to platform module
+#print(help(platform))
+
+
+
+
